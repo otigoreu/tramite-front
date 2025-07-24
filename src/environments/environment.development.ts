612161b1afb9f543af56f21ee7734116862af3c5
otigoreu <eponce@regionucayali.gob.pe>
2025-07-24 16:55:31 -0500

    nuevo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  //baseUrl: 'https://localhost:7230',
-  baseUrl: 'https://goreuapi.regionucayali.gob.pe'
+  baseUrl: 'https://localhost:7230',
+  //baseUrl: 'https://goreuapi.regionucayali.gob.pe'
 
 };
