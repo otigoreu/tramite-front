9bb4b8a5ec0bc0c9952442763aebdaafb72c12ef
otigoreu <eponce@regionucayali.gob.pe>
2025-07-24 14:38:37 -0500

    github pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl: 'https://localhost:7230',
+  //baseUrl: 'https://localhost:7230',
   // baseUrlFirma: 'https://localhost:3010',
-  //baseUrl: 'https://tramiteapi.regionucayali.gob.pe'
+  baseUrl: 'https://goreuapi.regionucayali.gob.pe'
 };
